efa783c2fee352968783cfade44b6424faeb8bd5
Jacob Herper <jacob@herper.uk>
2022-01-25 20:54:14 +0000

    updates contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
 
+const dotenv = require("dotenv");
+dotenv.config();
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -18,4 +21,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.0",
+  networks: {
+    rinkeby: {
+      url: process.env.ALCHEMY_API_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,21 +1,34 @@
 const main = async () => {
-  // compile contract and generate artefacts
   const nftContractFactory = await hre.ethers.getContractFactory("MyEpicNFT");
-  // hardhat creates local Ethereum network for this contract
   const nftContract = await nftContractFactory.deploy();
-  // wait for contract to be mined and deployed to local blockchain
   await nftContract.deployed();
   console.log("Contract deployed to:", nftContract.address);
 
-  // Call the function
+  // Call the function.
   let txn = await nftContract.makeAnEpicNFT();
-  // Wait for it to be mined
+  // Wait for it to be mined.
   await txn.wait();
+  console.log("Minted NFT #1");
 
-  // Mint another NFT for fun
   txn = await nftContract.makeAnEpicNFT();
-  // Wait for it to be mined
+  // Wait for it to be mined.
   await txn.wait();
+  console.log("Minted NFT #2");
+
+  txn = await nftContract.makeAnEpicNFT();
+  // Wait for it to be mined.
+  await txn.wait();
+  console.log("Minted NFT #3");
+
+  txn = await nftContract.makeAnEpicNFT();
+  // Wait for it to be mined.
+  await txn.wait();
+  console.log("Minted NFT #4");
+
+  txn = await nftContract.makeAnEpicNFT();
+  // Wait for it to be mined.
+  await txn.wait();
+  console.log("Minted NFT #5");
 };
 
 const runMain = async () => {
